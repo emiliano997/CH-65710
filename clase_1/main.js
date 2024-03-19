2b58336c034f14d4b4709e55a8416be02930ff4e
Emiliano Perez <117744347+emiliano997@users.noreply.github.com>
2024-03-19 20:51:25 -0300

    [ADD] Clase 1

---
+++ clase_1/main.js
@@ -0,0 +1,30 @@
+// let = Valor variable
+let nombre = ""; // string / Cadena de texto
+let edad = 19; // Number / Numero
+
+nombre = "Rodrigo";
+// nombre = 43123;
+console.log(typeof edad);
+
+// Ejemplo concatenación
+let mensajeBienvenida = "Bienvenido " + nombre + " de edad " + edad;
+
+console.log(mensajeBienvenida);
+
+// const = Valor constante
+
+const year = 2024;
+const PI = 3.14;
+
+console.log(typeof year);
+
+alert("Primera clase Coder Flex");
+
+nombre = prompt("Ingrese su nombre");
+
+let confirmo = confirm("¿Deseas valorar bien la clase?");
+
+console.log(typeof nombre);
+console.log(typeof confirmo);
+
+alert("¿El alumno " + nombre + " valoró la clase bien? " + confirmo);
